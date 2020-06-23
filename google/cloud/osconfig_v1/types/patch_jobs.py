ece087c529cba8a75b3ec6f181cd6166ad0e4fc7
yoshi-automation <yoshi-automation@google.com>
2020-06-23 13:48:45 -0700

    docs: Fix several broken links in reference documentation
    PiperOrigin-RevId: 317658441
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Mon Jun 22 08:26:49 2020 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: 02c23d62c7a133608fcf6ea33aea90ed0e0d098a
    Source-Link: https://github.com/googleapis/googleapis/commit/02c23d62c7a133608fcf6ea33aea90ed0e0d098a

--- google/cloud/osconfig_v1/types/patch_jobs.py
+++ google/cloud/osconfig_v1/types/patch_jobs.py
@@ -685,7 +685,10 @@ class PatchInstanceFilter(proto.Message):
         instances (Sequence[str]):
             Targets any of the VM instances specified. Instances are
             specified by their URI in the form
-            ``zones/[ZONE]/instances/[INSTANCE_NAME],``\ projects/[PROJECT_ID]/zones/[ZONE]/instances/[INSTANCE_NAME]\ ``, or``\ https://www.googleapis.com/compute/v1/projects/[PROJECT_ID]/zones/[ZONE]/instances/[INSTANCE_NAME]\`
+            ``zones/[ZONE]/instances/[INSTANCE_NAME]``,
+            ``projects/[PROJECT_ID]/zones/[ZONE]/instances/[INSTANCE_NAME]``,
+            or
+            ``https://www.googleapis.com/compute/v1/projects/[PROJECT_ID]/zones/[ZONE]/instances/[INSTANCE_NAME]``
         instance_name_prefixes (Sequence[str]):
             Targets VMs whose name starts with one of
             these prefixes. Similar to labels, this is
