2d7525c9120ce634092c4b9c8ad972cd7d9916b5
yoshi-automation <yoshi-automation@google.com>
2020-08-01 08:44:04 -0700

    changes without context
            autosynth cannot find the source of changes triggered by earlier changes in this
            repository, or by version upgrades to tools such as linters.

--- google/cloud/osconfig_v1/services/os_config_service/async_client.py
+++ google/cloud/osconfig_v1/services/os_config_service/async_client.py
@@ -374,7 +374,7 @@ class OsConfigServiceAsyncClient:
         # This method is paged; wrap the response in a pager, which provides
         # an `__aiter__` convenience method.
         response = pagers.ListPatchJobsAsyncPager(
-            method=rpc, request=request, response=response
+            method=rpc, request=request, response=response, metadata=metadata
         )
 
         # Done; return the response.
@@ -454,7 +454,7 @@ class OsConfigServiceAsyncClient:
         # This method is paged; wrap the response in a pager, which provides
         # an `__aiter__` convenience method.
         response = pagers.ListPatchJobInstanceDetailsAsyncPager(
-            method=rpc, request=request, response=response
+            method=rpc, request=request, response=response, metadata=metadata
         )
 
         # Done; return the response.
@@ -710,7 +710,7 @@ class OsConfigServiceAsyncClient:
         # This method is paged; wrap the response in a pager, which provides
         # an `__aiter__` convenience method.
         response = pagers.ListPatchDeploymentsAsyncPager(
-            method=rpc, request=request, response=response
+            method=rpc, request=request, response=response, metadata=metadata
         )
 
         # Done; return the response.
