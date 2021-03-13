b6a8e3bde1a0c16f347e35079ad2d398761efea8
yoshi-automation <yoshi-automation@google.com>
2021-03-13 04:46:18 -0800

    changes without context
            autosynth cannot find the source of changes triggered by earlier changes in this
            repository, or by version upgrades to tools such as linters.

--- google/cloud/osconfig_v1/services/os_config_service/client.py
+++ google/cloud/osconfig_v1/services/os_config_service/client.py
@@ -792,6 +792,7 @@ class OsConfigServiceClient(metaclass=OsConfigServiceClientMeta):
                 -  Must end with a number or a letter.
                 -  Must be unique within the project.
 
+
                 This corresponds to the ``patch_deployment_id`` field
                 on the ``request`` instance; if ``request`` is provided, this
                 should not be set.
