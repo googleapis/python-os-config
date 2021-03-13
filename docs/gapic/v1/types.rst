cf9f071ff18fcdbac02029dae16ed08ec26ccc59
yoshi-automation <yoshi-automation@google.com>
2021-03-13 04:51:33 -0800

    chore: use python microgenerator rules for Cloud APIs
    PiperOrigin-RevId: 350604597
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Thu Jan 7 11:33:23 2021 -0800
    Source-Repo: googleapis/googleapis
    Source-Sha: 3131ab57860adbb25dfa78858e16b69ffc868f99
    Source-Link: https://github.com/googleapis/googleapis/commit/3131ab57860adbb25dfa78858e16b69ffc868f99

--- docs/gapic/v1/types.rst
+++
@@ -1,5 +0,0 @@
-Types for OS Config API Client
-==============================
-
-.. automodule:: google.cloud.osconfig_v1.types
-    :members:
--- google/cloud/osconfig_v1/__init__.py
+++ google/cloud/osconfig_v1/__init__.py
@@ -1,45 +1,91 @@
 # -*- coding: utf-8 -*-
-#
+
 # Copyright 2020 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     https://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
-
-from __future__ import absolute_import
-import sys
-import warnings
-
-from google.cloud.osconfig_v1 import types
-from google.cloud.osconfig_v1.gapic import enums
-from google.cloud.osconfig_v1.gapic import os_config_service_client
-
-
-if sys.version_info[:2] == (2, 7):
-    message = (
-        "A future version of this library will drop support for Python 2.7. "
-        "More details about Python 2 support for Google Cloud Client Libraries "
-        "can be found at https://cloud.google.com/python/docs/python2-sunset/"
-    )
-    warnings.warn(message, DeprecationWarning)
-
-
-class OsConfigServiceClient(os_config_service_client.OsConfigServiceClient):
-    __doc__ = os_config_service_client.OsConfigServiceClient.__doc__
-    enums = enums
+from .services.os_config_service import OsConfigServiceClient
+from .types.inventory import Inventory
+from .types.osconfig_common import FixedOrPercent
+from .types.patch_deployments import CreatePatchDeploymentRequest
+from .types.patch_deployments import DeletePatchDeploymentRequest
+from .types.patch_deployments import GetPatchDeploymentRequest
+from .types.patch_deployments import ListPatchDeploymentsRequest
+from .types.patch_deployments import ListPatchDeploymentsResponse
+from .types.patch_deployments import MonthlySchedule
+from .types.patch_deployments import OneTimeSchedule
+from .types.patch_deployments import PatchDeployment
+from .types.patch_deployments import RecurringSchedule
+from .types.patch_deployments import WeekDayOfMonth
+from .types.patch_deployments import WeeklySchedule
+from .types.patch_jobs import AptSettings
+from .types.patch_jobs import CancelPatchJobRequest
+from .types.patch_jobs import ExecStep
+from .types.patch_jobs import ExecStepConfig
+from .types.patch_jobs import ExecutePatchJobRequest
+from .types.patch_jobs import GcsObject
+from .types.patch_jobs import GetPatchJobRequest
+from .types.patch_jobs import GooSettings
+from .types.patch_jobs import Instance
+from .types.patch_jobs import ListPatchJobInstanceDetailsRequest
+from .types.patch_jobs import ListPatchJobInstanceDetailsResponse
+from .types.patch_jobs import ListPatchJobsRequest
+from .types.patch_jobs import ListPatchJobsResponse
+from .types.patch_jobs import PatchConfig
+from .types.patch_jobs import PatchInstanceFilter
+from .types.patch_jobs import PatchJob
+from .types.patch_jobs import PatchJobInstanceDetails
+from .types.patch_jobs import PatchRollout
+from .types.patch_jobs import WindowsUpdateSettings
+from .types.patch_jobs import YumSettings
+from .types.patch_jobs import ZypperSettings
 
 
 __all__ = (
-    "enums",
-    "types",
+    "AptSettings",
+    "CancelPatchJobRequest",
+    "CreatePatchDeploymentRequest",
+    "DeletePatchDeploymentRequest",
+    "ExecStep",
+    "ExecStepConfig",
+    "ExecutePatchJobRequest",
+    "FixedOrPercent",
+    "GcsObject",
+    "GetPatchDeploymentRequest",
+    "GetPatchJobRequest",
+    "GooSettings",
+    "Instance",
+    "Inventory",
+    "ListPatchDeploymentsRequest",
+    "ListPatchDeploymentsResponse",
+    "ListPatchJobInstanceDetailsRequest",
+    "ListPatchJobInstanceDetailsResponse",
+    "ListPatchJobsRequest",
+    "ListPatchJobsResponse",
+    "MonthlySchedule",
+    "OneTimeSchedule",
+    "PatchConfig",
+    "PatchDeployment",
+    "PatchInstanceFilter",
+    "PatchJob",
+    "PatchJobInstanceDetails",
+    "PatchRollout",
+    "RecurringSchedule",
+    "WeekDayOfMonth",
+    "WeeklySchedule",
+    "WindowsUpdateSettings",
+    "YumSettings",
+    "ZypperSettings",
     "OsConfigServiceClient",
 )
